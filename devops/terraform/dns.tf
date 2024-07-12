aba07eb2c8d2e82b4917fe9b79397715c273790e
Wawrzyniec 'Wawrzek' Niewodniczański <wawrzek@ivynet.dev>
2024-07-12 12:52:34 +0100

    Fix LB  (with DNS, HeathCheck, SGs)

--- devops/terraform/dns.tf
+++ devops/terraform/dns.tf
@@ -1,3 +1,14 @@
 resource "aws_route53_zone" "test" {
   name = "test.projectglove.io"
 }
+
+resource "aws_route53_record" "enclave" {
+  zone_id = aws_route53_zone.test.zone_id
+  name    = "enclave.test.projectglove.io"
+  type    = "A"
+  alias {
+    name                   = aws_lb.enclave.dns_name
+    zone_id                = aws_lb.enclave.zone_id
+    evaluate_target_health = true
+  }
+}
--- devops/terraform/lb.tf
+++ devops/terraform/lb.tf
@@ -1,18 +1,43 @@
 resource "aws_lb" "enclave" {
-  name               = "enclave"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb_enclave.id]
-  subnets            = [for s in data.aws_subnets.default.ids: s]
-
+  name                       = "enclave"
+  internal                   = false
+  load_balancer_type         = "application"
+  security_groups            = [aws_security_group.lb_enclave.id]
+  subnets                    = [for s in data.aws_subnets.default.ids : s]
   enable_deletion_protection = true
-
   /*  access_logs {
     bucket  = aws_s3_bucket.lb_logs.id
     prefix  = "test-lb"
     enabled = true
   }
 */
+}
+
+resource "aws_lb_listener" "enclave" {
+  load_balancer_arn = aws_lb.enclave.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.enclave.arn
+  }
+}
+
+resource "aws_lb_target_group" "enclave" {
+  name     = "enclave"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+  health_check {
+    path = "/info"
+  }
+
+}
+
+resource "aws_lb_target_group_attachment" "enclave" {
+  target_group_arn = aws_lb_target_group.enclave.arn
+  target_id        = aws_instance.enclave.id
+  port             = 8080
 }
 
 resource "aws_security_group" "lb_enclave" {
@@ -26,7 +51,7 @@ resource "aws_security_group_rule" "http" {
   protocol          = "tcp"
   from_port         = 80
   to_port           = 80
-  cidr_blocks         = ["0.0.0.0/0"]
+  cidr_blocks       = ["0.0.0.0/0"]
   description       = "HTTP web traffic"
   security_group_id = aws_security_group.lb_enclave.id
 }
@@ -36,7 +61,7 @@ resource "aws_security_group_rule" "https" {
   protocol          = "tcp"
   from_port         = 443
   to_port           = 443
-  cidr_blocks         = ["0.0.0.0/0"]
+  cidr_blocks       = ["0.0.0.0/0"]
   description       = "HTTPS web traffic"
   security_group_id = aws_security_group.lb_enclave.id
 }
@@ -46,7 +71,7 @@ resource "aws_security_group_rule" "all" {
   protocol          = "-1"
   from_port         = 0
   to_port           = 0
-  cidr_blocks         = ["0.0.0.0/0"]
+  cidr_blocks       = ["0.0.0.0/0"]
   description       = "All egress"
   security_group_id = aws_security_group.lb_enclave.id
 }
